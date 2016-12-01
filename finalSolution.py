c053e122717d292d0b4ac809a1eabc2ae5449c4b
ChristopherCoffey <christophercoffey89@outlook.com>
2016-12-01 17:45:19 +0000

    finished

--- finalSolution.py
+++ finalSolution.py
@@ -1,3 +1,4 @@
+import json
 import urllib3
 from model import Model
 
@@ -16,8 +17,7 @@ def process_data(flag):
         print("Data fetched from url!")
     for i in range(len(data)):
         data[i] = data[i].strip().split(', ')
-
-    train_classifier(data)
+    return data
 
 def train_classifier(data):
 
@@ -96,10 +96,16 @@ def train_classifier(data):
 
     lessSalary = calculateAvg(lessSalary)
     moreSalary = calculateAvg(moreSalary)
-    print("After average, less = ", lessSalary, '\n\n', 'more =', moreSalary)
+    # print("After average, less = ", lessSalary, '\n\n', 'more =', moreSalary)
     classSepVal = calculateClassSep(lessSalary, moreSalary, classSepVal)
-
+    writeToFile(classSepVal)
     print('Class seperation value is', classSepVal)
+
+def writeToFile(cls):
+
+    with open('classificationData.json', 'w') as f:  # Open file in write mode
+        json.dump(cls, f)
+
 
 def calculateAvg(d):
     
@@ -109,6 +115,7 @@ def calculateAvg(d):
                 d[k][k1] = d[k][k1] / d[k]['total']
     return d
 
+
 def calculateClassSep(lSal, mSal, cls):
 
     for k, v in lSal.items():
@@ -117,7 +124,148 @@ def calculateClassSep(lSal, mSal, cls):
                         k == 'Capital-loss' or \
                         k == 'Hours-per-week':
             cls[k][k + '-class-sep'] = (lSal[k]['total-' + k] + mSal[k]['total-' + k]) / 2.0
-
+        else:
+            for k1, v1 in lSal[k].items():
+                if lSal[k][k1] > mSal[k][k1]:
+                    cls[k][k1] = '<=50K'
+                else:
+                    cls[k][k1] = '>50K'
     return cls
 
-process_data(1)  # 1 will fetch data locally, anything else will fetch it from the url
+
+def test_data(rows):
+
+    result = {}
+    lSal = 0
+    mSal = 0
+    with open('classificationData.json', 'r') as f:
+        cls = json.load(f)
+    compare = []
+    data = process_data(1)
+    if rows > (len(data) - 2):
+        print('You exceeded the total no of records dumbass')
+        return
+    for i in range(rows):
+
+        if data[i][0] != '?':
+            if int(data[i][0]) < cls['Age']['Age-class-sep']:
+                lSal += 1
+            else:
+                mSal += 1
+        if data[i][1] != '?':
+            if cls['Workclass'][data[i][1]] == '<=50K':
+                lSal += 1
+            else:
+                mSal += 1
+
+        if data[i][4] != '?':
+            if int(data[i][4]) < cls['Education']['Education-class-sep']:
+                lSal += 1
+            else:
+                mSal += 1
+
+        if data[i][5] != '?':
+            if cls['Marital-status'][data[i][5]] == '<=50K':
+                lSal += 1
+            else:
+                mSal += 1
+
+        if data[i][6] != '?':
+            if cls['Occupation'][data[i][6]] == '<=50K':
+                lSal += 1
+            else:
+                mSal += 1
+
+        if data[i][7] != '?':
+            if cls['Relationship'][data[i][7]] == '<=50K':
+                lSal += 1
+            else:
+                mSal += 1
+
+        if data[i][8] != '?':
+            if cls['Race'][data[i][8]] == '<50K':
+                lSal += 1
+            else:
+                mSal += 1
+
+        if data[i][9] != '?':
+            if cls['Sex'][data[i][9]] == '<50K':
+                lSal += 1
+            else:
+                mSal += 1
+
+        if data[i][10] != '?':
+            if int(data[i][10]) < cls['Capital-gain']['Capital-gain-class-sep']:
+                lSal += 1
+            else:
+                mSal += 1
+
+        if data[i][11] != '?':
+            if int(data[i][11]) < cls['Capital-loss']['Capital-loss-class-sep']:
+                lSal += 1
+            else:
+                mSal += 1
+
+        if data[i][12] != '?':
+            if int(data[i][12]) < cls['Hours-per-week']['Hours-per-week-class-sep']:
+                lSal += 1
+            else:
+                mSal += 1
+
+        if lSal > mSal:
+            result = '<=50K'
+        elif lSal < mSal:
+            result = '>50k'
+        else:
+            isaccurate = 'Can\'t Say'
+        if result == data[i][14]:
+            isaccurate = 1
+        else:
+            isaccurate = 0
+
+        compare.append({
+            'isaccurate': isaccurate,
+            'lSal': lSal,
+            'mSal': mSal
+            })
+        lSal = 0
+        mSal = 0
+
+    sum = 0
+    count = 0
+    for score in compare:
+        if score['isaccurate'] != 'Can\'t Say':
+            sum = sum + score['isaccurate']
+            count += 1
+
+    print('Accuracy of the classifier is ', (sum/count * 100.0),'%')
+
+while True:
+    print('************** MENU **************')
+    print('Press 1 to train classifier')
+    print('Press 2 to test classifier')
+    print('Press 0 to exit')
+    try:
+        choice = int(input('Enter your choice...'))
+    except ValueError:
+        print('Invalid entry, try again')
+    if choice == 1:
+        try:
+            ch = int(input('Enter 1 to fetch the data locally, '
+                             'any other integer input will fetch training set from url '))
+        except ValueError:
+            print('Invalid entry, try again')
+        data = process_data(ch)  # 1 will fetch data locally, anything else will fetch it from the url
+        train_classifier(data)
+    elif choice == 2:
+        try:
+            rows = int(input('Enter the number of records you\'d like to use '
+                         'for testing the classifier '))
+        except ValueError:
+            print('Invalid entry, try again')
+        test_data(rows)
+    elif choice != 0:
+        print('Invalid choice, Try again')
+    else:
+        print('Bye!')
+        break
